86180095aee557f03df5aa8f4fc1a2d9baf6c153
OndrejS <ondrejsevcak@gmail.com>
2024-07-08 00:44:03 +0200

    base tag

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -193,7 +193,7 @@
       "url": "icon-512.png"
     },
     {
-      "hash": "sha256-WRcgti1D2Dtt0b2dYhexegEUhYtdbkwbXzEbyJzZW\/w=",
+      "hash": "sha256-6Y9AqNeH7in5HDz+eNzYfl1SvQTRb\/fIfgzW\/kzRLV8=",
       "url": "index.html"
     },
     {
@@ -205,5 +205,5 @@
       "url": "sample-data\/weather.json"
     }
   ],
-  "version": "qDJu5i1f"
+  "version": "YddWrgSG"
 };
