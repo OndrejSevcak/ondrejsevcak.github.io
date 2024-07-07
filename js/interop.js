8300071c9bc6b10aa7ce42bb5ad38a09e4267d9d
OndrejS <ondrejsevcak@gmail.com>
2024-07-08 00:13:39 +0200

    first

---
+++ js/interop.js
@@ -0,0 +1,10 @@
+﻿window.interopFunctions = {
+
+    getRowCol: function (element) {
+        return {
+            row: element.getAttribute('data-row'),
+            col: element.getAttribute('data-col')
+        };
+    }
+
+}
