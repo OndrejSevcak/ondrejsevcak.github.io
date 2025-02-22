284b6da2b65a0cdf9447701f4983384aa9f2d2e2
OndrejS <ondrejsevcak@gmail.com>
2025-02-22 20:34:21 +0100

    new solution published

--- js/modules.js
+++ js/modules.js
@@ -3,3 +3,22 @@
         hljs.highlightBlock(elm);
     })
 }
+
+export function replaceCssClass(toBeReplaced, replacement) {
+    const elements = document.querySelectorAll(`.${toBeReplaced}`);
+    elements.forEach((elm) => {
+        elm.classList.remove(toBeReplaced);
+        elm.classList.add(replacement);
+    });
+}
+
+export function applyPostStyling() {
+    const h3s = document.getElementsByTagName('h3');
+    if (h3s) {
+        Array.from(h3s).forEach(e => {
+            e.style = 'margin-top: 25px;';
+        });
+    }
+}
+
+
